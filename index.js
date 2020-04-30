8a860d14b2ba178b5dfa6c7da5641076203e904c
Felipe Saa <ext_figsaa@falabella.cl>
2020-04-30 11:42:46 -0400

    return headers and body

--- index.js
+++ index.js
@@ -12,21 +12,16 @@ expressWinston.requestWhitelist.push('body')
 app.use(
   expressWinston.logger({
     transports: [new winston.transports.Console(), new winston.transports.File({filename: 'combined.log'})],
-    format: winston.format.combine(winston.format.colorize(), winston.format.json(), winston.format.prettyPrint())
+    format: winston.format.combine(winston.format.colorize(), winston.format.json(), winston.format.prettyPrint()),
   })
 )
-
-const responseTypesByRequestType = {
-  ProductoModificarRequest: 'ProductoModificarResponse',
-  ProductoCrearRequest: 'ProductoCrearResponse'
-}
 
 app.use('*', (req, res) => {
   console.log('\n' + '=========='.repeat(15))
   console.log('=========='.repeat(15))
-  const responseKey = responseTypesByRequestType[Object.keys(req.body)[0]] || 'data'
   res.send({
-    [responseKey]: {Body: req.body}
+    headers: req.headers,
+    body: req.body,
   })
 })
 
